3096d2e727e014dbf1953701e61b09690d8acb71
rachel2bester <rachel2bester@gmail.com>
2022-05-18 15:00:19 +0200

    wrote pseudo code for script, added files

---
+++ script.js
@@ -0,0 +1,23 @@
+//
+
+
+//split string into array
+
+const solve = (array) => {
+    //find brackets
+    //for each bracket, 
+    //  solve(whats in bracket)
+    //  replace whats in bracket on main array
+    //
+    //for each indice
+    //  solve indice using Math.pow
+    //  replace whats in main array
+    // 
+    // for each multiplication and division, left to right
+    //  solve using * and /
+    //  replace that part of main array
+    //  
+    // for each addition and subtraction, left to right
+    //  solve using + and -
+    //  replace that part of main array
+}
