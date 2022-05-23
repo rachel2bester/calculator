72676dbcecfdafe8985984af76bcd1aa62f30257
rachel2bester <rachel2bester@gmail.com>
2022-05-23 16:16:24 +0200

    fixed brackets

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let arr = [];
 const onPressNum = (event) => {
     console.log(Number(event.target.innerText) + " button pressed")
 
-    if (arr != [] && !isNaN(arr[arr.length - 1])) { 
+    if (arr != [] && (!isNaN(arr[arr.length - 1]) || arr[arr.length - 1] === "-")) { 
         arr[arr.length - 1] = arr[arr.length - 1] + event.target.innerText;
     } else {
         arr.push(event.target.innerText);
@@ -23,9 +23,29 @@ const onPressNum = (event) => {
 }
 
 const onPressOperator = (event) => {
-    console.log(Number(event.target.innerText) + " button pressed")
-    
-    arr.push(event.target.innerText);
+    let toAdd = event.target.innerText;
+    console.log(toAdd + " button pressed");
+    if (toAdd === "√") {
+        arr.push(2, "rt", "(");
+
+    } else if (toAdd === "y\n√") {
+        arr.push("rt", "(");
+
+    }else if (toAdd === "x\n2") {
+        arr.push("po", "(", 2, ")");
+
+    }  else if (toAdd === "x\ny") {
+        arr.push("po", "(");
+
+    }  else if (toAdd === "-") {
+        arr.push("+-");
+
+    }  else if (toAdd === "(-)") {
+        arr.push("-");
+
+    } else {
+        arr.push(toAdd);
+    }
     
     updateInput();
 }
@@ -39,6 +59,8 @@ const onDelete = (event) => {
     }
     updateInput();
 }
+
+
 
 const onEquals = (event) => {
     arr.forEach((str) => {
@@ -63,14 +85,35 @@ const onClear = (event) => {
 
 const updateInput = () => {
     let string = "";
-    arr.forEach((item) => {
-        string = string +  " " + item;
-    });
-    input.innerText = string;
+    console.log(arr)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "po") {
+            string = string + ` <span class="indice">`;
+            
+        } else if (arr[i] === ")" && (arr.slice(0,i)[arr.lastIndexOf("(") - 1] === "po")) {
+            console.log(") here");
+            string = string +  ` ${arr[i]}</span>`;
+        
+        
+        } else if (arr[i] === "rt") {
+            if (arr[i - 1] === 2) {
+                string = string  +  ` √`;
+            } else {
+                string = string  +  `√`;
+            }
+        } else if (arr[i + 1] == "rt") {
+            string = string + ` <span class="indice">${arr[i]}</span>`;
+        } else if (arr[i] == "+-") {
+            string = string + " -"
+        } else {
+            string = string +  " " + arr[i];
+        }
+    }
+    input.innerHTML = `<p>${string}</p>`;
 }
 
 
-//one.addEventListener("click", onPress1);
+
 
 numbers.forEach((number) => number.addEventListener("click", onPressNum));
 operator.forEach((number) => number.addEventListener("click", onPressOperator));
@@ -78,28 +121,15 @@ clear.addEventListener("click", onClear);
 del.addEventListener("click", onDelete);
 eq.addEventListener("click", onEquals);
 
-//split string into array
-
-//var numsAndOperators = ["(", 5, "+", 1, ")", "*", 2];
 
 
-
-
-
-
-
-
-
-
-
-let numsAndOperators = [5, "^", "2", "+", 1, "*", 2];
 
 
 const solve = (arr) => {
     let numsAndOperators = [...arr];
-    //find brackets
 
-    //find last open bracket
+    
+
     while (numsAndOperators.includes("(")) {
         const lastOpenIndex = numsAndOperators.lastIndexOf("(");
         console.log(lastOpenIndex)
@@ -111,30 +141,41 @@ const solve = (arr) => {
         console.log(solutionIn);
         numsAndOperators = numsAndOperators.slice(0, lastOpenIndex).concat(solutionIn,numsAndOperators.slice(nextClose+1,numsAndOperators.length));
     }
+    console.log(numsAndOperators.join(" "));
 
 
 
     //for each indice, left to right
     for (let i = 0; i < numsAndOperators.length; i++) {
         let replace = null;
-        if(numsAndOperators[i] === "^") {
+        if(numsAndOperators[i] === "po") {
             replace = Math.pow(numsAndOperators[i - 1], numsAndOperators[i + 1]);
+            numsAndOperators = numsAndOperators.slice(0, i-1).concat([replace],numsAndOperators.slice(i+2,numsAndOperators.length))
+        }
+        if(numsAndOperators[i] === "rt") {
+            replace = Math.pow(numsAndOperators[i + 1], 1/numsAndOperators[i - 1]);
             numsAndOperators = numsAndOperators.slice(0, i-1).concat([replace],numsAndOperators.slice(i+2,numsAndOperators.length))
         }
     }
 
 
     // for each multiplication and division, left to right
+
     for (let i = 0; i < numsAndOperators.length; i++) {
-        
+            
         //if two numbers next to eachother
         if (!isNaN(numsAndOperators[i]) && !isNaN(numsAndOperators[i + 1])) {
             numsAndOperators[i] = numsAndOperators[i] * numsAndOperators[i + 1];
             numsAndOperators.splice(i + 1, 1);
+        }
+    }
 
-        } else {
+    while (numsAndOperators.includes("x") || numsAndOperators.includes("÷")) {
+        
+        for (let i = 0; i < numsAndOperators.length; i++) {
+
             let replace = null;
-            
+
             if(numsAndOperators[i] === "x") {
                 replace = numsAndOperators[i - 1] * numsAndOperators[i + 1];
             } else if (numsAndOperators[i] === "÷") {
@@ -142,29 +183,36 @@ const solve = (arr) => {
             }
     
             if (replace != null) {
-                numsAndOperators = numsAndOperators.slice(0, i-1).concat([replace],numsAndOperators.slice(i+2,numsAndOperators.length))
+                numsAndOperators = numsAndOperators.slice(0, i - 1).concat([replace],numsAndOperators.slice(i+2,numsAndOperators.length))
             }
-        }
-
-        
+            
+        }  
     }
     
 
     // for each addition and subtraction, left to right
-    for (let i = 0; i < numsAndOperators.length; i++) {
-        let replace = null;
-        if(numsAndOperators[i] === "+") {
-            replace = (Number)(numsAndOperators[i - 1]) + (Number)(numsAndOperators[i + 1]);
-        } else if (numsAndOperators[i] === "-") {
-            replace = numsAndOperators[i - 1] - numsAndOperators[i + 1];
-        }
+    while (numsAndOperators.includes("+") || numsAndOperators.includes("+-")) {
+        for (let i = 0; i < numsAndOperators.length; i++) {
+            let replace = null;
+            if(numsAndOperators[i] === "+") {
+                replace = (Number)(numsAndOperators[i - 1]) + (Number)(numsAndOperators[i + 1]);
+            } else if (numsAndOperators[i] === "+-") {
+                replace = numsAndOperators[i - 1] - numsAndOperators[i + 1];
+            }
 
-        if (replace != null) {
-            numsAndOperators = numsAndOperators.slice(0, i-1).concat([replace],numsAndOperators.slice(i+2,numsAndOperators.length))
+            if (replace != null) {
+                numsAndOperators = numsAndOperators.slice(0, i-1).concat([replace],numsAndOperators.slice(i+2,numsAndOperators.length))
+            }
         }
     }
 
-    return numsAndOperators;
+    if (numsAndOperators.length === 1) {
+        return numsAndOperators;
+    } else {
+        console.log("error" + numsAndOperators);
+        return NaN;
+    }
+    
 }
-//console.log(solve(numsAndOperators));
+
 
